4548dabbcb1773702ad7597d95091727f43dbcd2
Nodirkhan2003 <134544443+nodirkhan2003@users.noreply.github.com>
2026-05-31 16:27:26 +0500

    Fix TemplateResponse keyword arguments

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ Text:
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.post("/api/generate")
